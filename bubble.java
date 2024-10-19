95f07db2f62ca68162c170ec86d0ab1b776cc8a4
nissansah1 <loveman2073@gmail.com>
2024-10-20 03:58:54 +0530

    save commit

--- bubble.java
+++ bubble.java
@@ -42,45 +42,48 @@
 
 
 
+
+
+
 /*worst case of bubble sort */
-import java.util.Scanner;
+// import java.util.Scanner;
 
- public class  bubble{
-    public static void main(String[] args) {
+//  public class  bubble{
+//     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+//         Scanner sc = new Scanner(System.in);
             
-            /*enter size of an array*/
-            System.out.print("Please enter a arr size number: ");
-            int size = sc.nextInt();
+//             /*enter size of an array*/
+//             System.out.print("Please enter a arr size number: ");
+//             int size = sc.nextInt();
 
-            /*declare array outside of loop with size assign* and store value */           
-            int Arr[]= new int[size];
+//             /*declare array outside of loop with size assign* and store value */           
+//             int Arr[]= new int[size];
             
-            for (int i=0; i<Arr.length; i++) {
-                /*value taken upto Arr.length*/
-                System.out.print("Please enter a " + i  + " number: ");
-                Arr[i] = sc.nextInt();  
-            }
+//             for (int i=0; i<Arr.length; i++) {
+//                 /*value taken upto Arr.length*/
+//                 System.out.print("Please enter a " + i  + " number: ");
+//                 Arr[i] = sc.nextInt();  
+//             }
 
-            for(int i=0 ; i<size-1; i++){
-                for(int j=0 ; j<size-1; j++){
-                    for(int k=0 ; k<size-i-j; k++){               
+//             for(int i=0 ; i<size-1; i++){
+//                 for(int j=0 ; j<size-1; j++){
+//                     for(int k=0 ; k<size-i-j; k++){               
 
-                        if (Arr[j]>Arr[j+k]){
-                            int temp= Arr[j+k];
-                            Arr[j+k]=Arr[j];
-                            Arr[j]=temp;
-                     }
-                   } 
-                }            
-            }
-            for (int i=0; i<Arr.length; i++) {
-            System.out.print (" "+ Arr[i]); 
-            } 
-            System.out.println();
+//                         if (Arr[j]>Arr[j+k]){
+//                             int temp= Arr[j+k];
+//                             Arr[j+k]=Arr[j];
+//                             Arr[j]=temp;
+//                      }
+//                    } 
+//                 }            
+//             }
+//             for (int i=0; i<Arr.length; i++) {
+//             System.out.print (" "+ Arr[i]); 
+//             } 
+//             System.out.println();
 
-        sc.close();
-    }
-}
+//         sc.close();
+//     }
+// }
 
